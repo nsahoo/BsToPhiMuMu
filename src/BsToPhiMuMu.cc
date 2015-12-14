b15ae67634d26a3741cd0f776c91fb2c1d413201
nsahoo <niladribihari.sahoo@cern.ch>
2015-12-14 15:49:41 +0100

    updated edanalyzer

--- src/BsToPhiMuMu.cc
+++ src/BsToPhiMuMu.cc
@@ -10,28 +10,164 @@
  Implementation:
      [Notes on implementation]
 */
-//
+//====================================================================
 // Original Author:  Niladribihari Sahoo,42 3-024,+41227662373,
+//       copyright @ N.Sahoo, NISER, Bhubaneswar
 //         Created:  Sat Nov 28 07:33:44 CET 2015
+//====================================================================
 // $Id$
 //
 //
 
 
-// system include files
+//-----------------------                                                                                                                                           
+// system include files                                                                                                                                                       
+//-----------------------                                                                                                                                              
 #include <memory>
 
-// user include files
+//----------------------                                                                                                                                                     
+// user include files                                                                                                                                                          
+//----------------------                                                                                                                                               
+    
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
-
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-//
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Common/interface/TriggerNames.h"
+
+#include "MagneticField/Engine/interface/MagneticField.h"
+#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
+
+#include "DataFormats/Common/interface/Handle.h"
+#include "DataFormats/Common/interface/TriggerResults.h"
+#include "DataFormats/BeamSpot/interface/BeamSpot.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/PatCandidates/interface/CompositeCandidate.h"
+#include "DataFormats/PatCandidates/interface/GenericParticle.h"
+#include "DataFormats/Candidate/interface/VertexCompositeCandidate.h"
+#include "DataFormats/Candidate/interface/VertexCompositeCandidateFwd.h"
+#include "DataFormats/RecoCandidate/interface/RecoCandidate.h"
+#include "DataFormats/RecoCandidate/interface/RecoChargedCandidate.h"
+#include "DataFormats/RecoCandidate/interface/RecoChargedCandidateFwd.h"
+
+#include "HLTrigger/HLTcore/interface/HLTConfigProvider.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrack.h"
+#include "TrackingTools/PatternTools/interface/ClosestApproachInRPhi.h"
+
+#include "RecoVertex/KinematicFitPrimitives/interface/ParticleMass.h"
+#include "RecoVertex/KinematicFitPrimitives/interface/RefCountedKinematicParticle.h"
+#include "RecoVertex/KinematicFitPrimitives/interface/TransientTrackKinematicParticle.h"
+#include "RecoVertex/KinematicFitPrimitives/interface/KinematicParticleFactoryFromTransientTrack.h"
+#include "RecoVertex/KinematicFit/interface/KinematicParticleVertexFitter.h"
+#include "RecoVertex/KinematicFit/interface/KinematicParticleFitter.h"
+#include "RecoVertex/KinematicFit/interface/MassKinematicConstraint.h"
+#include "RecoVertex/KinematicFit/interface/KinematicConstrainedVertexFitter.h"
+#include "RecoVertex/VertexTools/interface/VertexDistance3D.h"
+
+#include "CommonTools/Statistics/interface/ChiSquaredProbability.h"
+
+#include <TFile.h>
+#include <TTree.h>
+#include <TMath.h>
+#include <TLorentzVector.h>
+#include <TH1.h>
+
+
+using namespace std;
+
+
+const int MUONMINUS_PDG_ID = 13;
+const int KAONPLUS_PDG_ID = 321;
+const int PHI_PDG_ID = 333;      // phi(1020)
+const int BS_PDG_ID = 531;
+const int JPSI_PDG_ID = 443;
+const int PSI2S_PDG_ID = 100443;
+
+const double PI = 3.141592653589793;
+
+
+//----------------
+//  structures
+//----------------
+struct HistArgs{
+  char name[128];
+  char title[128];
+  int n_bins;
+  double x_min;
+  double x_max;
+};
+
+enum HistName{
+  h_events,
+  h_mupt,
+  h_mueta,
+  h_mumdcabs,
+  h_mumutrkr,
+  h_mumutrkz,
+
+  h_mumudca,
+  h_mumuvtxcl,
+  h_mumupt,
+  h_mumumass,
+  h_mumulxybs,
+
+  h_mumucosalphabs,
+  h_trkpt,
+  h_trkdcasigbs,
+  h_bsvtxchisq,
+  h_bsvtxcl,
+
+  h_phimass,
+  h_bsmass,                                                                                                                           
+
+  kHistNameSize
+};
+
+//--------------------                                                                                                                                                            
+// Global hist args                                                                                                                                                             
+//--------------------                                                                                                                                                          
+HistArgs hist_args[kHistNameSize] = {
+  // name, title, n_bins, x_min, x_max                                                                                                                                          
+
+  {"h_events", "Processed Events", 1, 0, 1},
+  {"h_mupt", "Muon pT; [GeV]", 100, 0, 30},
+  {"h_mueta", "Muon eta", 100, 0, 3},
+  {"h_mumdcabs", "#mu^{-} DCA beam spot; DCA [cm]", 100, 0, 10},
+  {"h_mumutrkr", "#mu^{+}#mu^{-} distance in phi-eta; [cm]", 100, 0, 50},
+  {"h_mumutrkz", "#mu^{+}#mu^{-} distance in Z; [cm]", 100, 0, 100},
+
+  {"h_mumudca",  "#mu^{+}#mu^{-} DCA; [cm]", 100, 0, 20},
+  {"h_mumuvtxcl",  "#mu^{+}#mu^{-} vertex CL", 100, 0, 1},
+  {"h_mumupt",    "#mu^{+}#mu^{-} pT ; pT [GeV]", 100, 0, 50},
+  {"h_mumumass", "#mu^{+}#mu^{-} invariant mass; M(#mu^{+}#mu^{-}) [GeV/c^{2}]",
+   100, 2, 20},
+  {"h_mumulxybs", "#mu^{+}#mu^{-} Lxy #sigma beam spot", 100, 0, 100},
+
+  {"h_mumucosalphabs", "#mu^{+}#mu^{-} cos #alpha beam spot", 100, 0, 1},
+  {"h_trkpt", "Pion track pT; pT [GeV]", 100, 0, 20},
+  {"h_trkdcasigbs", "Pion track DCA/#sigma beam spot; DCA/#sigma", 1000, 0, 100},
+  {"h_bsvtxchisq", "B_{s} decay vertex chisq", 100, 0, 1000},
+  {"h_bsvtxcl", "#B_{s} decay vertex CL", 100, 0, 1},
+
+  {"h_phimass", "#phi(1020) mass; M(KK) [GeV/^{2}]", 100, 0, 20},   
+  {"h_bsmass", "B_{s} mass; M(KK#mu#mu}) [GeV]", 100, 0, 20},                                                                                           
+
+};
+
+//--------------------                                                                                                                                                         
+// Define histograms                                                                                                                                                         
+//--------------------                                                                                                                                                           
+TH1F *histos[kHistNameSize];
+
+
+//-----------------------
 // class declaration
-//
+//-----------------------
 
 class BsToPhiMuMu : public edm::EDAnalyzer {
    public:
@@ -50,6 +186,36 @@ class BsToPhiMuMu : public edm::EDAnalyzer {
       virtual void endRun(edm::Run const&, edm::EventSetup const&);
       virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
       virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+
+
+
+  bool buildBsToPhiMuMu(const edm::Event &);
+
+  void calLS (double, double, double, double, double, double, double,
+              double, double,  double, double, double, double, double,
+              double, double, double, double, double*, double*);
+
+  void calCosAlpha (double, double, double, double, double,
+                    double, double, double, double, double,
+                    double, double, double, double,
+                    double, double, double, double,
+                    double*, double*);
+
+  void calCosAlpha2d (double, double, double, double, double,
+                      double, double, double, double, double,
+                      double, double, double, double,
+                      double, double, double, double,
+                      double*, double*);
+
+  void calCtau(RefCountedKinematicTree, double &, double &);
+  double calEta(double, double, double);
+  double calPhi(double, double, double);
+  double calEtaPhiDistance (double, double, double, double, double, double);
+  void clearVariables();
+
+  bool hasBeamSpot(const edm::Event&);
+
+
 
       // ----------member data ---------------------------
 };
